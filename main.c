49810b4ff7c284838486f8b4a476cad3a0116a69
Maxim Dankovtsev <max.dankow@gmail.com>
2015-04-22 15:04:49 +0300

    BackUP

--- main.c
+++ main.c
@@ -33,6 +33,16 @@ size_t file_length;
 size_t tasks_num;
 int semaphores;
 
+void* mount_shm(int shm_key)
+{
+    void* shm_ptr = shmat(shm_key, NULL, 0);
+    if (shm_ptr == (void*) -1)
+    {
+        perror("shmap");
+        exit(EXIT_FAILURE);
+    }
+    return shm_ptr;
+}
 
 /*
 Возвращает идентификатор разделяемой памяти,
@@ -63,12 +73,7 @@ int read_file(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
-    char* data = shmat(shm_key, NULL, 0);
-    if (data == (void*) -1)
-    {
-        perror("shmap");
-        exit(EXIT_FAILURE);
-    }
+    char* data = (char*) mount_shm(shm_key);
 
     int real_file = open(argv[1], O_RDONLY, 0777);
     read(real_file, data, file_length);
@@ -119,12 +124,7 @@ int init_tasks(int file_shm_key)
     }
 
   //разместим задачи в разделяемой памяти
-    ssize_t* tasks_mem = shmat(shm_key, NULL, 0);
-    if (tasks_mem == (void*) -1)
-    {
-        perror("shmap");
-        exit(EXIT_FAILURE);
-    }
+    ssize_t* tasks_mem = (ssize_t*) mount_shm(shm_key);
     memcpy(tasks_mem, tasks, tasks_num * sizeof(ssize_t));
     printf("Tasks are:\n");
     for (size_t i = 0; i < tasks_num; ++i)
@@ -198,10 +198,39 @@ char* process_string(char* str, int child_id)
     printf("(%d) result is: %s\n", child_id, result);
 }
 
+int init_result_buffer(void)
+{
+  //создаем область разделяемой памяти для результатов
+  //shmget гарантирует инициалиизацию нулями
+    int shm_key = shmget(IPC_PRIVATE, 
+      //указатель на конец текущего буфера
+        sizeof(char*)
+      //для каждого задания указатель на начало и конец результата в буфере
+        + tasks_num * 2 * sizeof(char*) 
+      //сам буфер, в два раза больше, чем исходный файл
+        + file_length * 2, 
+        IPC_CREAT | 0660);
+
+    if (shm_key == -1)
+    {
+        perror("shmget");
+        exit(EXIT_FAILURE);
+    }
+
+    return shm_key;
+}
+
+void write_result_to_buffer(size_t task_id, char* answer)
+{
+    semop(semaphores, &sop_write_res_lock, 2);
+    semop(semaphores, &sop_write_res_unlock, 1);
+}
+
 int main(int argc, char** argv)
 {
     int file_shm_key = read_file(argc, argv);
     int tasks_shm_key = init_tasks(file_shm_key);
+    int result_buf = init_result_buffer();
     semaphores = init_semaphores();
 
     for (int i = 0; i < CHILDREN_NUMBER; ++i)
@@ -209,29 +238,22 @@ int main(int argc, char** argv)
         pid_t code = fork();
         if (code == 0)
         {
-            ssize_t* tasks_mem = shmat(tasks_shm_key, NULL, 0);
-            if (tasks_mem == (void*) -1)
-            {
-                perror("shmap");
-                exit(EXIT_FAILURE);
-            }
-            char* file_mem = shmat(file_shm_key, NULL, 0);
-            if (file_mem == (void*) -1)
-            {
-                perror("shmap");
-            } 
-            printf("CHILD\n");
+            ssize_t* tasks_mem = (ssize_t*) mount_shm(tasks_shm_key);
+            char* file_mem = (char*) mount_shm(file_shm_key); 
             ssize_t line_offset;
+
             do
             {
                 line_offset = get_next_task(tasks_mem);
+
                 printf("(%d) get %d - %s\n", i, 
                        line_offset, file_mem + line_offset);
+
                 process_string(file_mem + line_offset, i);
             }while(line_offset != NO_TASK);
+
             _exit(EXIT_SUCCESS);
         }
-        printf("PARENT\n");
     }
 
     for (int i = 0; i < CHILDREN_NUMBER; ++i)
